546888fbaf1d18724f5b8b2040962230ed174f69
asholejason <asholejason@126.com>
2017-01-20 16:07:20 +0800

    add Tutorial part 4

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,17 +1,10 @@
-// Register `phoneList` component, along with its associated controller and template
+// Register the `phoneList` component on the `phoneList` module,
 angular.
-module('phonecatApp').
+module('phoneList').
 component('phoneList', {
-    template:
-    '<ul>' +
-    '<li ng-repeat="phone in $ctrl.phones">' +
-    '<span>{{phone.name}}</span>' +
-    '<p>{{phone.snippet}}</p>' +
-    '</li>' +
-    '</ul>' +
-    '<p>Total number of phones: {{$ctrl.phones.length}}</p>',
-
+    templateUrl: "phone-list/phone-list.template.html",
     controller: function PhoneListController() {
+        //想法,这是一个json，可以从restful-api取得数据：this.phones = $http.get(api-url)
         this.phones = [
             {
                 name: 'Nexus S',
