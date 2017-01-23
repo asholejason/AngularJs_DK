10c614fd24ad22ceff10a327f461157d78d761ba
asholejason <asholejason@126.com>
2017-01-23 10:57:06 +0800

    add tutorial 5 code

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,6 +5,7 @@ component('phoneList', {
     templateUrl: "phone-list/phone-list.template.html",
     controller: function PhoneListController() {
         //想法,这是一个json，可以从restful-api取得数据：this.phones = $http.get(api-url)
+        this.query = document.getElementById('search_input').innerHTML;
         this.phones = [
             {
                 name: 'Nexus S',
